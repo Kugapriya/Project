12a40ea42f218a2276741b39b5badae87384ddd4
unknown <kugapriya1958@gmail.com>
2024-02-14 14:11:16 +0530

    first

--- src/main/java/com/BookStore/Books/BooksApplication.java
+++ src/main/java/com/BookStore/Books/BooksApplication.java
@@ -1,8 +1,8 @@
 package com.BookStore.Books;
-import com.BookStore.Books.book1;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
@@ -11,18 +11,19 @@ public class BooksApplication {
 
 	public static void main(String[] args) {
 
+
 		try {
 			ObjectMapper mapper = new XmlMapper();
 
-			FileInputStream inputStream = new FileInputStream(new File("D:\\Books\\src\\main\\java\\com\\BookStore\\Books\\Books.xml"));
+			FileInputStream inputStream = new FileInputStream(new File("D://Books//src//main//java//com//BookStore//Books//Books.xml"));
 			TypeReference<List<book1>> typeReference = new TypeReference<List<book1>>() {};
 			List<book1> booksList = mapper.readValue(inputStream, typeReference);
 			inputStream.close();
 			for(book1 abc:booksList)
 			{
-				System.out.println(abc.getTitle());
+			System.out.println(abc.getTitle());
 				abc.Database();
-			}
+		}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
--- src/main/java/com/BookStore/Books/book1.java
+++ src/main/java/com/BookStore/Books/book1.java
@@ -11,55 +11,66 @@ public class book1 {
     private String author;
     private int year;
     private double price;
-    public int getId() {
+    public int getId()
+    {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(int id)
+    {
         this.id = id;
     }
 
-    public String getTitle() {
+    public String getTitle()
+    {
         return title;
     }
 
-    public void setTitle(String title) {
+    public void setTitle(String title)
+    {
         this.title = title;
     }
 
-    public String getAuthor() {
+    public String getAuthor()
+    {
         return author;
     }
 
-
-    public void setAuthor(String author) {
+    public void setAuthor(String author)
+    {
         this.author = author;
     }
 
-    public int getYear() {
+    public int getYear()
+    {
         return year;
     }
 
-    public void setYear(int year) {
+    public void setYear(int year)
+    {
         this.year = year;
     }
 
-    public double getPrice() {
+    public double getPrice()
+    {
         return price;
     }
 
-    public void setPrice(double price) {
+    public void setPrice(double price)
+    {
         this.price = price;
     }
-    public void Database() {
+    public void Database()
+    {
+
         Connection connection = null;
         try {
 
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/users","root", "");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/kugapriya","root", "");
             if (connection != null) {
-                System.out.println("Connected");
 
-                String insertQuery = "INSERT INTO book (id, title, author, year,price) VALUES (?, ?, ?, ?,?)";
+
+                String insertQuery = "INSERT INTO book1 (id, title, author, year,price) VALUES (?, ?, ?, ?,?)";
 
 
                 PreparedStatement preparedStatement = connection.prepareStatement(insertQuery);
@@ -70,7 +81,6 @@ public class book1 {
                 preparedStatement.setDouble(5, price);
 
                 int rowsAffected = preparedStatement.executeUpdate();
-
             }
         } catch (SQLException e) {
             System.err.println("Failed to connect to the database!");

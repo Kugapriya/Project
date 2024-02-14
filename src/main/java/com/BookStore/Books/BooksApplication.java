4c4133fb4067d0af359a00643bda395fdc98ad26
unknown <kugapriya1958@gmail.com>
2024-02-14 10:55:14 +0530

    Project uploaded

---
+++ src/main/java/com/BookStore/Books/BooksApplication.java
@@ -0,0 +1,31 @@
+package com.BookStore.Books;
+import com.BookStore.Books.book1;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.List;
+public class BooksApplication {
+
+	public static void main(String[] args) {
+
+		try {
+			ObjectMapper mapper = new XmlMapper();
+
+			FileInputStream inputStream = new FileInputStream(new File("D:\\Books\\src\\main\\java\\com\\BookStore\\Books\\Books.xml"));
+			TypeReference<List<book1>> typeReference = new TypeReference<List<book1>>() {};
+			List<book1> booksList = mapper.readValue(inputStream, typeReference);
+			inputStream.close();
+			for(book1 abc:booksList)
+			{
+				System.out.println(abc.getTitle());
+				abc.Database();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
+
---
+++ src/main/java/com/BookStore/Books/book1.java
@@ -0,0 +1,90 @@
+package com.BookStore.Books;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+public class book1 {
+    private int id;
+    private String title ;
+    private String author;
+    private int year;
+    private double price;
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public void Database() {
+        Connection connection = null;
+        try {
+
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/users","root", "");
+            if (connection != null) {
+                System.out.println("Connected");
+
+                String insertQuery = "INSERT INTO book (id, title, author, year,price) VALUES (?, ?, ?, ?,?)";
+
+
+                PreparedStatement preparedStatement = connection.prepareStatement(insertQuery);
+                preparedStatement.setInt(1, id);
+                preparedStatement.setString(2, title);
+                preparedStatement.setString(3, author);
+                preparedStatement.setInt(4, year);
+                preparedStatement.setDouble(5, price);
+
+                int rowsAffected = preparedStatement.executeUpdate();
+
+            }
+        } catch (SQLException e) {
+            System.err.println("Failed to connect to the database!");
+            e.printStackTrace();
+
+        }
+    }
+    public  static void Database(List<book1> booksList)
+    {
+        for(book1 abc: booksList)
+        {
+            System.out.println(abc.getTitle());
+            abc.Database();
+        }
+    }
+}
+
